19f7168a0238e1a9971689d6c215b60ee794de02
Igor Camargos <igorgcam@gmail.com>
2018-11-03 20:55:20 -0300

    Inserido teste "falho", e resultado do teste exportado para XML

--- src/HomeTest.java
+++ src/HomeTest.java
@@ -7,6 +7,12 @@ class HomeTest {
     @Test
     void div() {
         int a = 10, b = 3;
-        assertEquals(3, Home.div(10,3));
+        assertEquals(3, Home.div(a,b));
+    }
+
+    @Test
+    void divErrado() {
+        int a = 10, b = 3;
+        assertEquals(5, Home.div(a,b));
     }
 }
